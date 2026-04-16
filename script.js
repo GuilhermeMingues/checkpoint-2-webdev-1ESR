f0efc063fbd275ed6aac751e88a1141c3618db1f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-15 23:02:15 -0300

    Questão 1

---
+++ script.js
@@ -0,0 +1,8 @@
+// QUESTÃO 1
+let tarefaConcluida = true;
+
+if (tarefaConcluida === true) {
+    console.log("A tarefa está concluída!");
+} else {
+    console.log("A tarefa não foi concluída");
+}
